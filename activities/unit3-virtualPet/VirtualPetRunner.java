81431b8abb682e6a38949e836aaaeb968d29dd12
Paschal <149417037+codepasky@users.noreply.github.com>
2025-12-14 22:04:24 -0500

    Add files via upload

---
+++ activities/unit3-virtualPet/VirtualPetRunner.java
@@ -0,0 +1,7 @@
+public class VirtualPetRunner {
+    public static void main(String[] args) {
+        
+        VirtualPet axel = new VirtualPet("Axel");
+        System.out.println(axel);
+    }
+}
