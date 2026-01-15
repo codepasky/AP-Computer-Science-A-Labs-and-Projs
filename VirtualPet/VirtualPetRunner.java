f3b6ed9a207838ccd8c037d853eed04db7b08a7f
Paschal <149417037+codepasky@users.noreply.github.com>
2026-01-15 10:20:57 -0500

    Delete VirtualPet directory

--- VirtualPet/VirtualPetRunner.java
+++
@@ -1,7 +0,0 @@
-public class VirtualPetRunner {
-    public static void main(String[] args) {
-        
-        VirtualPet axel = new VirtualPet("Axel");
-        System.out.println(axel);
-    }
-}
